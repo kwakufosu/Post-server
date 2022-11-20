de044863e7594e24db5d0046949ec5276b7c7def
kwaku fosu <kwakuboachiefosu@gmail.com>
2022-11-20 13:36:07 +0000

    fixed error bug

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const express = require('express');
 const http = require('http');
-const mongoose= require('mongoose')
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-const typeDefs= require('./schema/schema.graphql')
-const resolvers= require('./resolvers/resolvers')
+const typeDefs = require('./schema/schema.graphql');
+const resolvers = require('./resolvers/resolvers');
 const {
   ApolloServerPluginDrainHttpServer,
 } = require('@apollo/server/plugin/drainHttpServer');
@@ -17,10 +17,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const httpServer = http.createServer(app);
 
-
-
-
-
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -28,14 +24,17 @@ const startServer = async () => {
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await server.start();
-  await mongoose.connect(process.env.MONGODB,{useNewUrlParser:true,useUnifiedTopology:true},
-    (err)=>{
-        if (err){
-            console.log("error in connection",error)
-        }else{
-            console.log("mongodb is connected")
-        }
-    })
+  await mongoose.connect(
+    process.env.MONGODB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+      if (err) {
+        console.log('error in connection', err);
+      } else {
+        console.log('mongodb is connected');
+      }
+    }
+  );
   app.use('/graphql', json(), expressMiddleware(server));
   await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 
